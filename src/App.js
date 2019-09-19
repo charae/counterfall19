74d7ab53ccaab95462c2a216f3bd4fcfa929487f
first last <charaexo@icloud.com>
2019-09-18 19:33:25 -0600

    end of counter

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
 import React, { Component } from 'react';
+import Counter from './components/counter/Counter';
 
 class App extends Component {
+  // constructor(props) {
+  //   super(props)
+  //   this.state = { value: 0 }
+  //   this.inc = this.inc.bind(this)
+  //   this.dec = this.dec.bind(this)
+  // }
+  
+  render() {
+    return (
+      <div>
+        <h1 className='colored'>Counter Example</h1>
+        <Counter name= 'yellow' age='56'/>
+        <Counter name='purple'/>
+        <Counter name= 'blue'/>
+        <Counter name='black'/>
 
-
-  state = { value: 0 }
-
-  inc = () => {
-    this.setState(  (state)  => {
-      return { value: state.value + 1}
-    })
-    console.log(this.state.value)
+      </div>
+    )
   }
-
-  dec () {
-    this.setState ({ value: this.state.value - 1 })
-
-
-  }
-
- render() {
- return (
-   <div>
-   <h1 className='colored'>Counter Example</h1>
-   <p>{ this.state.value } </p>
-   <button onClick={this.inc}>+</button>
-   <button onClick={this.dec}>-</button>
-   </div>
-   )
- }
 }
+
+// React.createElement(h1, counter)React.createElement(h1, counter)
+// React.createElement(div, counter)
+
 export default App;
